3858b8c4e1dc91be8e53d019b88409369684ef78
Artemii131994 <artemii131994@gmail.com>
2021-06-04 21:18:42 +0400

    исправил

--- src/main/java/dao/UserDaoHibernateImpl.java
+++ src/main/java/dao/UserDaoHibernateImpl.java
@@ -11,171 +11,104 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS user" + "(id int not null auto_increment," + " name VARCHAR(50), " +
+            "lastname VARCHAR(50), " + "age int, " + "PRIMARY KEY (id))";
+    private static final String DELETE_TABLE = "DROP TABLE IF EXISTS user";
+    private static final String GET_ALL = "FROM User";
+    private static final String CLEAN_USER = "TRUNCATE TABLE user";
 
-    private final SessionFactory sessionFactory = Util.getSessionFactory();
+    private final SessionFactory sessionFactory = Util.getInstance().getSessionFactory();
 
     public UserDaoHibernateImpl(SessionFactory sessionFactory) {
 
     }
 
+    Session session = null;
+
     @Override
     public void createUsersTable() {
-        Session session = null;
         Transaction transaction = null;
-        try {
-            session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
-            session.createSQLQuery("CREATE TABLE IF NOT EXISTS user" +
-                    "(id int not null auto_increment," +
-                    " name VARCHAR(50), " +
-                    "lastname VARCHAR(50), " +
-                    "age int, " +
-                    "PRIMARY KEY (id))").executeUpdate();
+            session.createSQLQuery(CREATE_TABLE).executeUpdate();
             transaction.commit();
 
         } catch (Exception e) {
+            transaction.rollback();
             e.printStackTrace();
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        } finally {
-            if (session != null) {
-                try {
-                    session.close();
-                } catch (HibernateException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 
+
     @Override
     public void dropUsersTable() {
-        Session session = null;
         Transaction transaction = null;
-        try {
-            session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
-            session.createSQLQuery("DROP TABLE IF EXISTS user").executeUpdate();
+            session.createSQLQuery(DELETE_TABLE).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
+            transaction.rollback();
             e.printStackTrace();
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        } finally {
-            if (session != null) {
-                try {
-                    session.close();
-                } catch (HibernateException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Session session = null;
         Transaction transaction = null;
-        try {
-            session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             User user = new User(name, lastName, age);
             session.save(user);
             transaction.commit();
             System.out.println("User с именем " + name + " добавлен базу данных");
         } catch (HibernateException e) {
+            transaction.rollback();
             e.printStackTrace();
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        } finally {
-            if (session != null) {
-                try {
-                    session.close();
-                } catch (HibernateException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = null;
         Transaction transaction = null;
-        try {
-            session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             User user = session.get(User.class, id);
             session.delete(user);
             transaction.commit();
         } catch (HibernateException e) {
+            transaction.rollback();
             e.printStackTrace();
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        } finally {
-            if (session != null) {
-                try {
-                    session.close();
-                } catch (HibernateException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 
     @Override
     public List<User> getAllUsers() {
-        Session session = null;
         Transaction transaction = null;
         List list = new ArrayList();
-        try {
-            session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
-            list = session.createQuery("FROM User").list();
+            list = session.createQuery(GET_ALL).list();
             transaction.commit();
         } catch (HibernateException e) {
+
+            transaction.rollback();
             e.printStackTrace();
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        } finally {
-            if (session != null) {
-                try {
-                    session.close();
-                } catch (HibernateException e) {
-                    e.printStackTrace();
-                }
-            }
-            return list;
         }
+        return list;
     }
+
 
     @Override
     public void cleanUsersTable() {
-        Session session = null;
         Transaction transaction = null;
-        try {
-            session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
-            session.createSQLQuery("TRUNCATE TABLE user").executeUpdate();
+            session.createSQLQuery(CLEAN_USER).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
+            transaction.rollback();
             e.printStackTrace();
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        } finally {
-            if (session != null) {
-                try {
-                    session.close();
-                } catch (HibernateException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 }
--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,6 +1,7 @@
 package util;
 
 import model.User;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -10,37 +11,48 @@ import org.hibernate.service.ServiceRegistry;
 import java.util.Properties;
 
 public class Util {
-    private static Util instance = null;
-    private static SessionFactory sessionFactory = null;
+    private static Util INSTANCE = null;
+    private static SessionFactory sessionFactory;
 
     private Util() {
 
     }
 
     public static SessionFactory getSessionFactory() {
-        if (instance == null) {
-            instance = new Util();
+        if (sessionFactory == null) {
 
-            Configuration configuration = new Configuration();
+            try {
+                Configuration configuration = new Configuration();
 
-            Properties settings = new Properties();
-            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-            settings.put(Environment.URL, "jdbc:mysql://localhost:3307/test");
-            settings.put(Environment.USER, "root");
-            settings.put(Environment.PASS, "root");
-            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-            settings.put(Environment.SHOW_SQL, "true");
-            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3307/test");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "root");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-            configuration.setProperties(settings);
-            configuration.addAnnotatedClass(User.class);
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
 
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
 
-            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (HibernateException e) {
+                e.printStackTrace();
+            }
         }
         return sessionFactory;
+    }
+
+
+    public static Util getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Util();
+        }
+        return INSTANCE;
     }
 
 
